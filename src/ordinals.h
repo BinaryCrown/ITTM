84c3f41be2045d384ae31cd7c74407ececc0a3ac
BinaryCrown <93054701+BinaryCrown@users.noreply.github.com>
2022-04-30 17:10:03 +0100

    Updated comment.

--- src/ordinals.h
+++ src/ordinals.h
@@ -10,7 +10,7 @@ namespace ordinals {
         public:
             // The ordinal = w^a1*b1 + w^a2*b2 + ... + w^an*bn
             // The vector "coefficients" = b1, b2, ..., bn
-            // exponents "exponents" = a1, a2, ..., an
+            // The vector "exponents" = a1, a2, ..., an
             vector<int> coefficients;  
             vector<CantorNF> exponents;
             bool std();
