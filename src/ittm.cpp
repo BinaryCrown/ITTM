17edb8536f9d6b4dd6d1128f6c6adf399e68d76d
BinaryCrown <93054701+BinaryCrown@users.noreply.github.com>
2022-05-01 17:59:58 +0100

    Added ITTM config

---
+++ src/ittm.cpp
@@ -0,0 +1,7 @@
+#include "ittm.h"
+#include<iostream>
+
+using namespace ITTM;
+using namespace std;
+
+// ITTM stuff goes here
---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include "ordinals.h"
+#include "ittm.h"
+
+using namespace ordinals;
+using namespace ITTM;
+
+int main() {
+    // Code goes here
+    return 0;
+}
