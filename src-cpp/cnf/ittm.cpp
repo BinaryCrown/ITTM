8deb636a3368318b8dac650f8d9811c440c60787
BinaryCrown <93054701+BinaryCrown@users.noreply.github.com>
2022-05-02 09:37:09 +0100

    Fixed tapes.

--- src-cpp/cnf/ittm.cpp
+++ src-cpp/cnf/ittm.cpp
@@ -4,7 +4,11 @@
 using namespace ITTM;
 using namespace std;
 
-void setVals_TR(int currentState, char currentCell, char newContent, int moveDirection, int nextState) {
+void getTapeEnt(vector<char> tape, int entry) {
+    return (entry < tape.size) ? tape[entry] : "_"; 
+}
+
+void TransitionRule::setVals_TR(int currentState, char currentCell, char newContent, int moveDirection, int nextState) {
     if (currentState < -1 || nextState < -1) {
         throw std::invalid_argument("Received invalid transition rule state.");
     }
@@ -18,7 +22,7 @@ void setVals_TR(int currentState, char currentCell, char newContent, int moveDir
     this->nextState = nextState;
 }
 
-void setVals_ITTM(std::vector<TransitionRule> ITTMRuleset, std::vector<char> initialTape) {
+void ITTM::setVals_ITTM(std::vector<TransitionRule> ITTMRuleset, std::vector<char> initialTape) {
     if (ITTMRuleset.size == 0) {
         throw std::invalid_argument("Received empty ITTM ruleset.")
     }
@@ -41,7 +45,7 @@ std::vector<char> ITTM::getTapeAtStep(ordinals::CantorNF step) {
     else {
         std::vector<char> previousTape = getStateAtStep(step.fundamentalSeq(0));
         int previousState = this.getStateAtStep(step.fundamentalSeq(0));
-        int previousSymbol = previousTape[getReadHeadPosAtStep(step.fundamentalSeq(0))];
+        int previousSymbol = getTapeEnt(previousTape, getReadHeadPosAtStep(step.fundamentalSeq(0)))
         for (unsigned i = 0; i < (this->ITTMRuleset).size; i++) {
             if ((this->ITTMRuleset)[i].currentState == previousState && (this->ITTMRuleset)[i].currentCell == previousSymbol) {
                 break;
@@ -51,8 +55,11 @@ std::vector<char> ITTM::getTapeAtStep(ordinals::CantorNF step) {
                 throw std::invalid_argument("No transition rule available.")
             }
         }
-        std::vector<char> newTape = previousTape;
-        newTape[getReadHeadPosAtStep(step.fundamentalSeq(0))] == (this->ITTMRuleset)[i].newContent;
+        std::vector<char> newTape{};
+        for (unsigned i = 0; i <= getReadHeadPosAtStep(step.fundamentalSeq(0)); i++) {
+            char k = (i < getReadHeadPosAtStep(step.fundamentalSeq(0))) ? "_" : (this->ITTMRuleset)[i].newContent);
+            newTape.push_back((i < previousTape.size) ? previousTape[i] : k);
+        }
         return newTape;
     }
 }
@@ -65,7 +72,7 @@ int ITTM::getStateAtStep(ordinals::CantorNF step) {
     if (isLim(step)) {return -1;}
     else {
         int previousState = this.getStateAtStep(step.fundamentalSeq(0));
-        int previousSymbol = currentTape[getReadHeadPosAtStep(step.fundamentalSeq(0))];
+        int previousSymbol = getTapeEnt(currentTape, getReadHeadPosAtStep(step.fundamentalSeq(0)));
         int previousState = this.getStateAtStep(step.fundamentalSeq(0));
         for (unsigned i = 0; i < (this->ITTMRuleset).size; i++) {
             if ((this->ITTMRuleset)[i].currentState == previousState && (this->ITTMRuleset)[i].currentCell == previousSymbol) {
@@ -86,7 +93,7 @@ int ITTM::getReadHeadPosAtStep(ordinals::CantorNF step) {
         std::vector<char> previousTape = getStateAtStep(step.fundamentalSeq(0));
         int previousPos = getReadHeadPosAtStep(step.fundamentalSeq(0));
         int previousState = this.getStateAtStep(step.fundamentalSeq(0));
-        int previousSymbol = previousTape[previousPos];
+        int previousSymbol = getTapeEnt(previousTape, previousPos);
         for (unsigned i = 0; i < (this->ITTMRuleset).size; i++) {
             if ((this->ITTMRuleset)[i].currentState == previousState && (this->ITTMRuleset)[i].currentCell == previousSymbol) {
                 break;
