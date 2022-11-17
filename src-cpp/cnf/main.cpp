f0f1b2d60cc804c75afadb169a000089e23e3b7e
Jayde 3 <93054701+BinaryCrown@users.noreply.github.com>
2022-11-17 15:25:24 +0000

    Worked a bit.

--- src-cpp/cnf/main.cpp
+++ src-cpp/cnf/main.cpp
@@ -1,13 +1,33 @@
 #include "ordinals.h"
 #include "ittm.h"
 #include<iostream>
+#include<fstream>
 #include<vector>
 #include<string>
 
 using namespace ordinals;
 using namespace ITTM;
+using namespace std;
+
+TransitionRule parseRule() {
+
+}
 
 int main() {
-    // Code goes here
+    std::string fileaddress;
+    std::cout << "Address of ITTM code: ";
+    cin >> fileaddress;
+    std::string command;
+    std::vector<string> commands;
+    ifstream CMDFile(fileaddress);
+    while (getline (CMDFile,command)) {
+        // Get the command from the file
+        commands.push_back(commands);
+    }
+    CMDFile.close();
+    // We've now gotten all the commands. Parsing time!
+    std::vector<TransitionRule> ITTMRuleset;
+    std::vector<char> initialTape;
+    ITTM sim(ITTMRuleset, initialTape);
     return 0;
 }
